6a12d6798260d6b4071bfc8f758eb55a73b8d7e0
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2013-03-27 17:47:42 -0500

    Switch featureCollection to an area of interest (Portugal)

--- src/geojson-grid.js
+++ src/geojson-grid.js
@@ -10,40 +10,13 @@ function init(){
             {layers: 'basic'} );
     map.addLayer(layer);
     map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
-    var featurecollection = {
-      "type": "FeatureCollection", 
-      "features": [
-        {"geometry": {
-            "type": "GeometryCollection", 
-            "geometries": [
-                {
-                    "type": "LineString", 
-                    "coordinates": 
-                        [[11.0878902207, 45.1602390564], 
-                        [15.01953125, 48.1298828125]]
-                }, 
-                {
-                    "type": "Polygon", 
-                    "coordinates": 
-                        [[[11.0878902207, 45.1602390564], 
-                          [14.931640625, 40.9228515625], 
-                          [0.8251953125, 41.0986328125], 
-                          [7.63671875, 48.96484375], 
-                          [11.0878902207, 45.1602390564]]]
-                },
-                {
-                    "type":"Point", 
-                    "coordinates":[15.87646484375, 44.1748046875]
-                }
-            ]
-        }, 
-        "type": "Feature", 
-        "properties": {}}
-      ]
-   };
+    var areaOfInterest = {"type":"FeatureCollection","features":[
+        {"type":"Feature","id":"PRT","properties":{"name":"Portugal"},"geometry":{"type":"Polygon","coordinates":[[[-9.034818,41.880571],[-8.671946,42.134689],[-8.263857,42.280469],[-8.013175,41.790886],[-7.422513,41.792075],[-7.251309,41.918346],[-6.668606,41.883387],[-6.389088,41.381815],[-6.851127,41.111083],[-6.86402,40.330872],[-7.026413,40.184524],[-7.066592,39.711892],[-7.498632,39.629571],[-7.098037,39.030073],[-7.374092,38.373059],[-7.029281,38.075764],[-7.166508,37.803894],[-7.537105,37.428904],[-7.453726,37.097788],[-7.855613,36.838269],[-8.382816,36.97888],[-8.898857,36.868809],[-8.746101,37.651346],[-8.839998,38.266243],[-9.287464,38.358486],[-9.526571,38.737429],[-9.446989,39.392066],[-9.048305,39.755093],[-8.977353,40.159306],[-8.768684,40.760639],[-8.790853,41.184334],[-8.990789,41.543459],[-9.034818,41.880571]]]}}
+        ]};
+
    var geojson_format = new OpenLayers.Format.GeoJSON();
    var vector_layer = new OpenLayers.Layer.Vector(); 
    map.addLayer(vector_layer);
-   vector_layer.addFeatures(geojson_format.read(featurecollection));
+   vector_layer.addFeatures(geojson_format.read(areaOfInterest));
 
 }

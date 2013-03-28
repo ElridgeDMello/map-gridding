7346652728869829ce78fb7f493ccf621bfe2962
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2013-03-28 15:40:20 -0500

    Add KML based vector layer.
    Used OL example as a starting source.

--- src/geojson-grid.js
+++ src/geojson-grid.js
@@ -1,6 +1,7 @@
-var lon = -97.8476;
-var lat = 30.4717;
-var zoom = 7;
+var lon = -112.169 ;//7.26332822974839140;
+var lat = 36.099;//8.35377187043956230;
+
+var zoom = 11; //7;
 var map, layer;
 
 function init(){
@@ -11,14 +12,21 @@ function init(){
     map.addLayer(layer);
     map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
 
-    var areaOfInterest =
-    {"type":"FeatureCollection","properties":{"kind":"state","state":"TX"},"features":[
-        {"type":"Feature","properties":{"kind":"county","name":"Travis","state":"TX"},"geometry":{"type":"MultiPolygon","coordinates":[[[[-97.9736,30.6251],[-97.9188,30.6032],[-97.9243,30.5703],[-97.8695,30.5484],[-97.8476,30.4717],[-97.7764,30.4279],[-97.5793,30.4991],[-97.3711,30.4170],[-97.4916,30.2089],[-97.6505,30.0719],[-97.6669,30.0665],[-97.7107,30.0226],[-98.1708,30.3567],[-98.1270,30.4279],[-98.0503,30.6251]]]]}}
-        ]};
-
-   var geojson_format = new OpenLayers.Format.GeoJSON();
-   var vector_layer = new OpenLayers.Layer.Vector(); 
+    var vector_layer = new OpenLayers.Layer.Vector(
+        'GridLayer',
+        {
+            protocol: new OpenLayers.Protocol.HTTP({
+                url: 'data/kml/lines.kml',
+                format: new OpenLayers.Format.KML({
+                    extractStyles: true,
+                    extractAttributes: true,
+                    maxDepth: 2
+                })
+            }),
+            strategies: [new OpenLayers.Strategy.Fixed()]        
+        }
+    ); 
    map.addLayer(vector_layer);
-   vector_layer.addFeatures(geojson_format.read(areaOfInterest));
+//   vector_layer.addFeatures(geojson_format.read(areaOfInterest));
 
 }
